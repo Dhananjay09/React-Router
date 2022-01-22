64341fd4a03bc240a0b309ef4003e89fba68587a
Dhananjay <dhananjay.singh@e2enetworks.com>
2022-01-22 12:06:27 +0530

    useRef

--- src/Components/Component1.js
+++ src/Components/Component1.js
@@ -1,15 +1,15 @@
-import {useState, useEffect} from 'react';
+import {useState, useRef} from 'react';
 const Component1 = () => {
     const [height, setHeight] = useState(0);
-    useEffect (()=> {
-    document.title = `${height}`
-    },[])
+    const currEl = useRef(null)
+    const onTextClicked = () =>{
+        currEl.current.focus();
+    }
     return (
         <>
-        <input onChange={(event)=> setHeight(event.target.value)} placeholder="Enter the input"></input>
-        <h1>Hello {height }</h1>
+        <input ref={currEl} onChange={(event)=> setHeight(event.target.value)} ></input>
+        <button onClick={onTextClicked}>Hello {height }</button>
         </>
-
     )
 }
 
