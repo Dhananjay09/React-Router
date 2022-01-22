40ea11d6f5ae8b5a79dda4689370c390848457ac
Dhananjay <dhananjay.singh@e2enetworks.com>
2022-01-22 12:36:05 +0530

    useReducer

--- src/Components/Component1.js
+++ src/Components/Component1.js
@@ -1,14 +1,29 @@
-import {useState, useRef} from 'react';
-const Component1 = () => {
-    const [height, setHeight] = useState(0);
-    const currEl = useRef(null)
-    const onTextClicked = () =>{
-        currEl.current.focus();
+import {useState, useRef, useReducer} from 'react';
+
+const updateState = (state, action) => {
+    switch(action){
+        case "INC":
+            state = {count : state.count + 1}
+            break
+        case "DEC":
+            state = { count : state.count - 1}
+            break
+        case "RST":
+            state = {count : 0}
+            break
+        default:
+            throw new Error("Error Fount in Switch")
     }
+return state;
+}
+const Component1 = () => {
+    const [state, dispatch] = useReducer(updateState , {count : 0});
     return (
         <>
-        <input ref={currEl} onChange={(event)=> setHeight(event.target.value)} ></input>
-        <button onClick={onTextClicked}>Hello {height }</button>
+        <h1> {state.count}</h1>
+        <button onClick={() => dispatch("INC")}>INCREMENT</button>
+        <button onClick={() => dispatch("DEC")}>DECREMENT</button>
+        <button onClick={() => dispatch("RST")}>RESET</button>
         </>
     )
 }
